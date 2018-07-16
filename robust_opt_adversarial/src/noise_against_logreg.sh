8632f6579a0189c4c6cdec07c8e2f029dd446c74
Kojin Oshiba <kojin.oshiba@gmail.com>
2018-07-15 19:03:16 -0700

    Edit bash scripts

--- robust_opt_adversarial/src/noise_against_logreg.sh
+++ robust_opt_adversarial/src/noise_against_logreg.sh
@@ -2,12 +2,5 @@
 for seed in {0..999}
 do
     echo $seed
-    bash -c '#!/bin/bash
-    #SBATCH -p shared
-    #SBATCH -o noise.%j.txt
-    #SBATCH -e noise.%j.err
-    #SBATCH -t 09:01:00
-    #SBATCH --mem 1000
-    #SBATCH -J noise
-    python noise_against_logreg.py --seed='${seed}
+    bash noise_against_logreg_helper.sh $seed
 done
